4ce92a6b166a026bf38c3a6afc7d2a350f5f2528
Necati Can Bozok <necaticanbozok@outlook.com>
2023-01-03 17:43:42 +0300

    [necaticanbozokRefactor][LoginFragment, SignUpFragment]

--- app/src/main/java/com/necatican/loginpage/ui/login/LoginFragment.kt
+++ app/src/main/java/com/necatican/loginpage/ui/login/LoginFragment.kt
@@ -1,15 +1,19 @@
 package com.necatican.loginpage.ui.login
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.necatican.loginpage.R
 import com.necatican.loginpage.base.BaseFragment
 import com.necatican.loginpage.databinding.FragmentLoginBinding
+import kotlinx.android.synthetic.main.fragment_login.*
 import kotlinx.android.synthetic.main.fragment_profile.*
 
 
@@ -20,49 +24,38 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::i
     private lateinit var firebaseAuth: FirebaseAuth
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        firebaseAuth = FirebaseAuth.getInstance()
 
-
-
-
+        val userMail = login_mail_editText.text.toString()
+        val userPassword = login_password_editText.text.toString()
 
         binding.loginbutton2.setOnClickListener {
             val userMail: String = binding.loginMailEditText.text.toString()
             val userPassword: String = binding.loginPasswordEditText.text.toString()
-            if (userMail.isEmpty()){
-                Toast.makeText(this.context,"Please Enter Your Email",Toast.LENGTH_SHORT).show()
-            }
-            if (userPassword.isEmpty()){
-                Toast.makeText(this.context,"Please Enter Your Password",Toast.LENGTH_SHORT).show()
+            if (userMail.isNotEmpty()) {
+                if (userPassword.isNotEmpty()) {
+                    Toast.makeText(this.context,"Login Successfull",Toast.LENGTH_SHORT).show()
 
-            }
-            database = FirebaseDatabase.getInstance().getReference("Users")
-            database.child(userName.toString()).get().addOnSuccessListener {
-            if (it.exists()) {
-                firebaseAuth.signInWithEmailAndPassword(userMail,
-                    userPassword
-                )
-                it.child("userMail").value
-                it.child("userPassword").value
-                Toast.makeText(this.context, "Login Successful", Toast.LENGTH_SHORT).show()
-                binding.loginMailEditText.text.clear()
-                binding.loginPasswordEditText.text.clear()
-            }
-        }
-        }
-                binding.loginSignupButton.setOnClickListener {
-                    findNavController().navigate(R.id.action_loginPage_to_signUpPage)
+                    findNavController().navigate(R.id.action_loginPage_to_mainPageFragment)
+
+                }else{
+                    Toast.makeText(this.context, "Please Enter Your Password", Toast.LENGTH_SHORT)
+                        .show()
                 }
 
+            }else{
+                Toast.makeText(this.context, "Please Enter Your Email", Toast.LENGTH_SHORT).show()
+
+            }
 
 
-
-
+        }
+        binding.loginSignupButton.setOnClickListener {
+            findNavController().navigate(R.id.action_loginPage_to_signUpPage)
+        }
     }
-
-
-
     override fun isNavigationBarVisible() = false
+
+
 }
 
 
--- app/src/main/java/com/necatican/loginpage/ui/login/SignUpFragment.kt
+++ app/src/main/java/com/necatican/loginpage/ui/login/SignUpFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.necatican.loginpage.R
@@ -21,54 +22,57 @@ class SignUpFragment : BaseFragment<FragmentSignUpBinding>(FragmentSignUpBinding
 
 
 
-binding.signupbutton2.setOnClickListener {
+        binding.signupbutton2.setOnClickListener {
 
-    val userName = binding.signupNameEditText.text.toString()
-    val userSurname = binding.signupSurnameEditText.text.toString()
-    val userMail = binding.signupMailEditText.text.toString()
-    val userPassword = binding.signupPasswordEditText.text.toString()
-    val confirmPass = binding.singupConfirmPassEditText.text.toString()
+            val userName = binding.signupNameEditText.text.toString()
+            val userSurname = binding.signupSurnameEditText.text.toString()
+            val userMail = binding.signupMailEditText.text.toString()
+            val userPassword = binding.signupPasswordEditText.text.toString()
+            val confirmPass = binding.singupConfirmPassEditText.text.toString()
 
-    if (userMail.isEmpty() && userPassword.isEmpty() && confirmPass.isEmpty()) {
-        Toast.makeText(this.context, "Please fill the empty fields", Toast.LENGTH_SHORT)
-            .show()
-        return@setOnClickListener
-    }
-    if (userPassword != confirmPass){
-        Toast.makeText(this.context,"Password must matched",Toast.LENGTH_SHORT)
-            .show()
-        return@setOnClickListener
-    }
-    firebaseAuth.createUserWithEmailAndPassword(userMail,userPassword)
+            if (userMail.isNotEmpty() && userPassword.isNotEmpty() && confirmPass.isNotEmpty()) {
+                if (userPassword == confirmPass){
 
+                    database = FirebaseDatabase.getInstance().getReference("Users")
+                    val User = User(userName, userSurname, userMail, userPassword , confirmPass)
+                    database.child(userName).setValue(User).addOnSuccessListener {
+                        binding.signupNameEditText.text.clear()
+                        binding.signupSurnameEditText.text.clear()
+                        binding.signupMailEditText.text.clear()
+                        binding.signupPasswordEditText.text.clear()
+                        binding.singupConfirmPassEditText.text.clear()
 
-    database = FirebaseDatabase.getInstance().getReference("Users")
-    val User = User(userName, userSurname, userMail, userPassword , confirmPass)
-    database.child(userName).setValue(User).addOnSuccessListener {
-        binding.signupNameEditText.text.clear()
-        binding.signupSurnameEditText.text.clear()
-        binding.signupMailEditText.text.clear()
-        binding.signupPasswordEditText.text.clear()
-        binding.singupConfirmPassEditText.text.clear()
+                        findNavController().navigate(R.id.action_signUpPage_to_loginPage)
 
 
 
 
+                    }
 
-    }
 
-    binding.signupbutton2.setOnClickListener {
-        findNavController().navigate(R.id.action_signUpPage_to_loginPage)
-    }
 
-}
+
+                }else{
+                    Toast.makeText(this.context,"Password must matched",Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }else{
+                Toast.makeText(this.context, "Please fill the empty fields", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+
+
+
+
+
+        }
         binding.signupLoginButton.setOnClickListener {
             findNavController().navigate(R.id.action_signUpPage_to_loginPage)
         }
 
 
-}
+    }
 
-override fun isNavigationBarVisible() = false
+    override fun isNavigationBarVisible() = false
 }
-
